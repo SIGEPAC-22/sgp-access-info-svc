c5a5550a2c6852dbaf63c07a2031bdfe7530dce9
SIGEPAC-2022 <114256674+SIGEPAC-2022@users.noreply.github.com>
2022-11-13 00:39:36 -0600

    GetInfoPersonal
    GetInfoPersonal

--- internal/getInfoPersonal/platform/storage/mysql/getInfoPersonalRepo.go
+++ internal/getInfoPersonal/platform/storage/mysql/getInfoPersonalRepo.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	kitlog "github.com/go-kit/log"
+	goconfig "github.com/iglin/go-config"
 	"sgp-access-info-svc/internal/getInfoPersonal"
 	"sgp-access-info-svc/kit/constants"
 )
@@ -20,7 +21,11 @@ func NewGetInfoPersonalRepository(db *sql.DB, logger kitlog.Logger) *GetInfoPers
 
 func (g *GetInfoPersonalRepository) GetInfoPersonalRepo(ctx context.Context, DocumentNumber string) (getInfoPersonal.GetInfoPersonalResponse, error) {
 
-	rowsSql := g.db.QueryRowContext(ctx, "select psipf.psi_first_name, psipf.psi_second_name, psipf.psi_first_last_name,\npsipf.psi_second_last_name, psipf.psi_date_of_birth, psipf.psi_document_number, psipf.psi_user, pssps.pss_gender_name, stdsd.std_state_name from \npsi_personal_information as psipf\ninner join pss_personal_sex as pssps\non pssps.pss_id = psipf.psi_sex_id\ninner join std_state_data as stdsd\non stdsd.std_id = psipf.psi_state_data_id\nwhere psi_document_number = ?;", DocumentNumber)
+	config := goconfig.NewConfig("./application.yaml", goconfig.Yaml)
+
+	statusActive := config.GetInt("app-properties.getComorbidity.idStatusActive")
+
+	rowsSql := g.db.QueryRowContext(ctx, "select psipf.psi_first_name, psipf.psi_second_name, psipf.psi_first_last_name,\npsipf.psi_second_last_name, psipf.psi_date_of_birth, psipf.psi_document_number, psipf.psi_user, pssps.pss_gender_name, stdsd.std_state_name from \npsi_personal_information as psipf\ninner join pss_personal_sex as pssps\non pssps.pss_id = psipf.psi_sex_id\ninner join std_state_data as stdsd\non stdsd.std_id = psipf.psi_state_data_id\nwhere psi_document_number = ? AND psi_state_data_id = ?;", DocumentNumber, statusActive)
 	var respDB SqlGetInfoPersonal
 	if err := rowsSql.Scan(&respDB.FirstName, &respDB.SecondName, &respDB.LastName, &respDB.MothersLast, &respDB.DateOfBirth, &respDB.DocumentNumber, &respDB.User, &respDB.Sex, &respDB.StateData); err != nil {
 		g.logger.Log("Data not found", constants.UUID, ctx.Value(constants.UUID))
